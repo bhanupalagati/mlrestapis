531c2e6de5007f01d2ae35eec70fd5e60ef87fa6
bhanupalagati <bhanupalagati@gmail.com>
2021-07-14 15:30:15 +0530

    adding pokemon clustering

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 from flask import Flask, jsonify
 from flask_restful import Api, Resource
 from covidcases.serve import CovidCases
+from pokemoncluster.serve import PokemonClusteredData, PokemonBorders, PokemonClassify
 
 app = Flask(__name__)
 api = Api(app)
 
 
 api.add_resource(CovidCases, "/covidcases")
+api.add_resource(PokemonClusteredData, "/pokemoncluster/data")
+api.add_resource(PokemonBorders, "/pokemoncluster/borders")
+api.add_resource(PokemonClassify, "/pokemoncluster/predict")
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ pokemoncluster/serve.py
@@ -0,0 +1,27 @@
+# This files purpose is to serve the poekmon Clustering information as a restAPI
+from flask_restful import Resource
+from flask import jsonify, request
+import pickle
+
+file_name = "pokemoncluster/pokemoncluster.pkl"
+open_file = open(file_name, "rb")
+# [clusteredData, model, maxDetails, minDetails]
+loaded_list = pickle.load(open_file)
+open_file.close()
+
+class PokemonClusteredData(Resource):
+    def get(self):
+        resp = loaded_list['clusterData']
+        return jsonify(resp)
+
+class PokemonBorders(Resource):
+    def get(self):
+        resp = {'maxDetails': loaded_list['maxDetails'], 'minDetails': loaded_list['minDetails']}
+        return jsonify(resp)
+
+class PokemonClassify(Resource):
+    def post(self):
+        model = loaded_list['model']
+        value = request.json['data']
+        prediction = model.predict([value])
+        return int(prediction[0])
